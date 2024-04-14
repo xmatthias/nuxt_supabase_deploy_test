ea00f9cd772eb42bb920b79878efebe9b371f725
Matthias <xmatthias@outlook.com>
2024-04-14 15:42:49 +0000

    nuxt ... globalThis

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,14 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/supabase'],
+  vite: {
+    optimizeDeps: {
+      esbuildOptions: {
+        // Node.js global to browser globalThis
+        define: {
+          global: 'globalThis'
+        },
+      }
+    }
+  }
 })
