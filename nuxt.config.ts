50e906e215c06bd0c106827bdefa1af2843dd81f
Matthias <xmatthias@outlook.com>
2024-04-14 15:46:00 +0000

    use Yarn setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/supabase'],
-  vite: {
-    optimizeDeps: {
-      esbuildOptions: {
-        // Node.js global to browser globalThis
-        define: {
-          global: 'globalThis'
-        },
-      }
-    }
-  }
+  modules: ['@nuxtjs/supabase']
 })
